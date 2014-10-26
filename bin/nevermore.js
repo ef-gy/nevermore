1dd9cd3f466420987d7b6bf45172e9a5a66e1feb
Magnus Achim Deininger <magnus@ef.gy>
2014-10-26 11:49:05 -0400

    first steps for the store function

--- bin/nevermore.js
+++ bin/nevermore.js
@@ -11,14 +11,21 @@ var crypto = require('crypto');
 function withHash (file, callback) {
   var hash = crypto.createHash(hashFunction);
   var fd = fs.createReadStream(file);
-  hash.setEncoding('hex');
+  hash.setEncoding('base64');
 
   fd.on('end', function() {
     hash.end();
-    callback(hash.read());
+    callback(hash.read().replace('/','-'));
   });
 
   fd.pipe(hash);
+}
+
+function splitHash(hash) {
+  return hashFunction + '/'
+       + hash.slice(0,2) + '/'
+       + hash.slice(2,4) + '/'
+       + hash.slice(4);
 }
 
 var ops = {
@@ -30,7 +37,23 @@ var ops = {
         });
       });
     },
-    'description': 'hash all operands and output the result to stdout.'
+    'arguments': [ '[files ...]' ],
+    'description': 'hash all operands and output the result to stdout'
+  },
+  'store': {
+    'f': function(operands) {
+      var datastore = operands[0];
+      operands.slice(1).forEach(function(file) {
+        withHash(file, function(hash){
+          var target = datastore
+                     + '/.neverstore/'
+                     + splitHash(hash);
+          console.log(file,'->',target);
+        });
+      });
+    },
+    'arguments': [ 'datastore', '[files ...]' ],
+    'desscription': 'store all files in the given datastore; create symlinks from source files'
   }
 }
 
