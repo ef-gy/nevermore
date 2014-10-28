a096598851126f1a14ab64c4943ad44fe2ed4d7e
Magnus Achim Deininger <magnus@ef.gy>
2014-10-28 09:56:59 -0400

    add some modules i'll need and put them to good use

--- bin/nevermore.js
+++ bin/nevermore.js
@@ -3,36 +3,14 @@
 var operation = process.argv[2];
 var operands  = process.argv.slice(3);
 
-var hashFunction = 'sha256';
-
-var fs = require('fs');
-var crypto = require('crypto');
-
-function withHash (file, callback) {
-  var hash = crypto.createHash(hashFunction);
-  var fd = fs.createReadStream(file);
-  hash.setEncoding('base64');
-
-  fd.on('end', function() {
-    hash.end();
-    callback(hash.read().replace('/','-'));
-  });
-
-  fd.pipe(hash);
-}
-
-function splitHash(hash) {
-  return hashFunction + '/'
-       + hash.slice(0,2) + '/'
-       + hash.slice(2,4) + '/'
-       + hash.slice(4);
-}
+var hash  = require('../lib/hash');
+var store = require('../lib/store');
 
 var ops = {
   'hash': {
     'f': function(operands) {
       operands.forEach(function(file) {
-        withHash(file, function(hash){
+        hash.with(file, function(hash){
           console.log(file,'~',hash);
         });
       });
@@ -41,17 +19,7 @@ var ops = {
     'description': 'hash all operands and output the result to stdout'
   },
   'store': {
-    'f': function(operands) {
-      var datastore = operands[0];
-      operands.slice(1).forEach(function(file) {
-        withHash(file, function(hash){
-          var target = datastore
-                     + '/.neverstore/'
-                     + splitHash(hash);
-          console.log(file,'->',target);
-        });
-      });
-    },
+    'f': store.store,
     'arguments': [ 'datastore', '[files ...]' ],
     'desscription': 'store all files in the given datastore; create symlinks from source files'
   }
